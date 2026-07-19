ce99f655328c2217a5d990ea4c3089e88e2ae677
Huang <huang@minview.local>
2026-07-19 20:59:00 +0800

    chore: clean up test/debug artifacts

--- build_test3.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-cd /d D:\Projects\minview-native
-cl /EHsc /Fe:test3.exe test_title3.cpp user32.lib /link /SUBSYSTEM:WINDOWS
-echo BUILD_%ERRORLEVEL%
